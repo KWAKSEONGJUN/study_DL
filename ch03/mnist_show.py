4fcdf805aefae6aa3b121c2ddbc8c9c0a5f722e9
kwakseongjun <sjman223@naver.com>
2021-04-26 17:29:56 +0900

    ch04

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -11,6 +11,7 @@ def img_show(img):
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(flatten=True, normalize=False)
 
+
 print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -3,17 +3,19 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import numpy as np
 from dataset.mnist import load_mnist
 import pickle
-from common.functions import sigmoid, softmax
+from common.functions import sigmoid, softmax, cross_entropy_error
 
 def get_data():
     (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
 
+
 def init_network():
     with open("ch03/sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
+
 
 def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
@@ -25,9 +27,10 @@ def predict(network, x):
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
-
+    
     return y
     
+
 x, t = get_data()
 network = init_network()
 
@@ -36,6 +39,8 @@ accuracy_cnt = 0
 for i in range(0, len(x), batch_size):
     x_batch = x[i:i+batch_size]
     y_batch = predict(network, x_batch)
-    p = np.argmax(y_batch, axis=1)
+    p = np.argmax(y_batch, axis=1) 
     accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
 print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
+
